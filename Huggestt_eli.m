f29d6b6aa37072a79d699c25676320c988ec271a
elisataveras <elisataveras@users.noreply.github.com>
2019-10-09 10:48:55 -0400

    updt program

--- Huggestt_eli.m
+++ Huggestt_eli.m
@@ -1,21 +1,20 @@
-clear 
+clear
 close all
-%%%% Set up parameters
-alpha = 0.35;
-beta = 0.994;
-delta = 0.025;
-sigma = 1.5;
-S= [0.97 0.03; 0.5 0.5];
-S_sta = S ^ 100;
+
+% PARAMETERS
+beta = .9932; % discount factor 
+sigma = 1.5; % coefficient of risk aversion
 b = 0.5; % replacement ratio (unemployment benefits)
-Y =  [1, b];
+y_s = [1, b]; % endowment in employment states
+PI = [.97 .03; .5 .5]; % transition matrix
 
-%%%% Set up discretized state space
-a_min = -2;
-a_max = 5;
-num_a = 10; % number of points in the grid for k
 
-a = linspace(a_min, a_max, num_a);
+% ASSET VECTOR
+a_lo = -2; %lower bound of grid points
+a_hi = 5;%upper bound of grid points
+num_a = 10;
+
+a = linspace(a_lo, a_hi, num_a); % asset (row) vector
 
 % INITIAL GUESS FOR q
 q_min = 0.98;
@@ -23,24 +22,38 @@ q_max = 1;
 q_guess = (q_min + q_max) / 2;
 
 
+%to do the calculations
+N = length(a);
+m = length(y_s);
+c_s=zeros(m*N,N);
+s = 1:m*N;
+s = reshape(s,N,m)';
+
+
+
 aggsav = 1 ;
 while abs(aggsav) >= 0.01 ;
 
-a_mat = repmat(a', [1 num_a]); % this will be useful in a bit
+    % CURRENT RETURN (UTILITY) FUNCTION
+    cons = bsxfun(@minus, a', q_guess * a);
+    cons = bsxfun(@plus, cons, permute(y_s, [1 3 2]));
+    ret = (cons .^ (1-sigma)) ./ (1 - sigma); % current period utility
+    
+% negative consumption is not possible -> make it irrelevant by assigning
+% it very large negative utility
+ret(cons < 0) = -Inf;
 
-%%%% Set up consumption and return function
-    c = bsxfun(@minus, a', q_guess * a);
-    c = bsxfun(@plus, c, permute(Y, [1 3 2]));
-ret = (c.^ (1 - sigma))./ (1 - sigma); % return function high
 
-% value function guess 
-v_guess = zeros(2,num_a);
-tol=1 ; 
-tic
-while tol > .0001  %& its < maxits
+
+    % INITIAL VALUE FUNCTION GUESS
+    v_guess = zeros(2, num_a);
+    
+    % VALUE FUNCTION ITERATION
+    v_tol = 1;
+ while v_tol >.0001;
     % compute the utility value for all possible combinations of k and k':
     value_fn = ret + beta * ...
-        repmat(permute((S * v_guess), [3 2 1]), [num_a 1 1]);
+        repmat(permute((PI * v_guess), [3 2 1]), [num_a 1 1]);
     
     
       % find the optimal k' for every k:
@@ -48,7 +61,7 @@ while tol > .0001  %& its < maxits
     vfn = shiftdim(vfn,2);
     
         % what is the distance between current guess and value function
-    d = max(abs( vfn(:) - v_guess(:) ));
+    v_tol = max(abs( vfn(:) - v_guess(:) ));
     
     % if distance is larger than tolerance, update current guess and
     % continue, otherwise exit the loop
@@ -56,21 +69,35 @@ while tol > .0001  %& its < maxits
     
 end
 
-    % Initial distribution
-    Mu= rand(T_sim, 1); % uniform distribution over s and A 
-
-  
-    % ITERATE OVER DISTRIBUTIONS
-    [emp_ind, a_ind, mass] = find(Mu > 0); % find non-zero indices
+    % KEEP DECSISION RULE
+    pol_fn = a(pol_indx);
     
-    MuNew = zeros(size(Mu));
-    for ii = 1:length(emp_ind)
-        apr_ind = pol_indx(emp_ind(ii), a_ind(ii)); % which a prime does the policy fn prescribe?
-        MuNew(:, apr_ind) = MuNew(:, apr_ind) + ... % which mass of households goes to which exogenous state?
-            (PI(emp_ind(ii), :) * mass)';
+    % Initial distribution
+    %guess an initial dstribution over s and a
+    % I am guessing this means dsitribution of people 
+    % zeros
+    Mu= rand(N*m,N*m,1); 
+    
+     MuNew = zeros(size(Mu));
+    for i = 1:m
+        MuNew(s(i,:),:)= kron(PI(i,:),sparse(1:N,pol_indx(s(i,:)),ones(1,N),N,N));
     end
- 
-        
+    MuNew = MuNew^1000;
+    mu = MuNew(1,:);
+    %This calculates the invariant distributions
+    err = dot(kron(ones(1,m),a),mu);
+  
+    % now I need to update
+    
+    if err>= 0.01% price is too low ; too much savinf
+        q_min = q_guess; %new q_min must be higher than before   
+    else
+        q_max = q_guess;   
+    end 
+    aggsav = q_max-q_min;
+    
+    
+   
     end
 
 
